0963584e4ed29a5c2bb7db72558d8ac136097799
ch8n <chetan.garg36@gmail.com>
2020-12-28 11:51:37 +0530

    mergecoverter

--- src/main/kotlin/katas/14.MorseCode.kt
+++ src/main/kotlin/katas/14.MorseCode.kt
@@ -1,4 +1,5 @@
 @file:Suppress("PackageDirectoryMismatch")
+
 package katas.morseCode
 
 
@@ -14,75 +15,102 @@ NOTE: Extra spaces before or after the code have no meaning and should be ignore
 
  */
 fun main() {
-    val signal = ".... . -.--  .--- ..- -.. ."
-    val humanReadable = decodeMorse2(signal)
+    //val signal = ".... . -.--  .--- ..- -.. ."
+
+    val morseResult = "big brain kotlin".toMorse()
+    println(morseResult)
+
+    val humanReadable = decodeMorse2(morseResult)
     println(humanReadable)
+
+}
+
+
+val morseCodeTranslator = mapOf<String, String>(
+        ".-" to "A",
+        "-..." to "B",
+        "-.-." to "C",
+        "-.." to "D",
+        "." to "E",
+        "..-." to "F",
+        "--." to "G",
+        "...." to "H",
+        ".." to "I",
+        ".---" to "J",
+        "-.-" to "K",
+        ".-.." to "L",
+        "--" to "M",
+        "-." to "N",
+        "---" to "O",
+        ".--." to "P",
+        "--.-" to "Q",
+        ".-." to "R",
+        "..." to "S",
+        "-" to "T",
+        "..-" to "U",
+        "...-" to "V",
+        ".--" to "W",
+        "-..-" to "X",
+        "-.--" to "Y",
+        "--.." to "Z",
+        ".----" to "1",
+        "..---" to "2",
+        "...--" to "3",
+        "....-" to "4",
+        "....." to "5",
+        "-...." to "6",
+        "--..." to "7",
+        "---.." to "8",
+        "----." to "9",
+        "-----" to "0",
+        "" to " ",
+        ".-.-.-" to ".",
+        "--..--" to ",",
+        "---..." to ".",
+        "..--.." to "?",
+        "-.-.--" to "!",
+        "...---..." to "SOS",
+        "-....-" to "''",
+        "-..-." to "/",
+        "-.--.-" to "()",
+        ".--.-." to "@",
+        "-...-" to "="
+)
+
+val morseCodeConverter: Map<String, String>
+    get() {
+        return morseCodeTranslator.entries.associate { (key, value) -> value.toLowerCase() to key }
+    }
+
+fun String.toMorse(): String {
+    val morseMapper = morseCodeConverter
+    val sentense = this
+    val words = sentense.split(" ")
+    val morse = words
+            .map { word ->
+                return@map word
+                        .toCharArray()
+                        .map { alpha -> morseMapper.get(alpha.toString()) }
+                        .joinToString(separator = " ")
+            }.joinToString(separator = "   ")
+    return morse
 }
 
 fun decodeMorse(code: String): String {
     val singalWords = code.split("   ")
     val morseChars = singalWords.map {
         it.split(" ")
-        .map { MorseCode(it) }
-        .joinToString("")
+                .map { morseCodeTranslator.get(it) }
+                .joinToString("")
     }.filter { it.isNotEmpty() }
     return morseChars.joinToString(" ")
 }
 
 fun decodeMorse2(code: String): String {
-    val singalWords = code.split("   ").also(::println)
-    return "TODO"
+    return code.split(" ")
+            .map { x -> morseCodeTranslator.get(x) ?: " " }
+            .joinToString("")
+            .replace("  ", " ")
+            .trim()
 }
 
-private fun MorseCode(morseCode: String): String? =
-    when (morseCode) {
-        ".-" -> "A"
-        "-..." -> "B"
-        "-.-." -> "C"
-        "-.." -> "D"
-        "." -> "E"
-        "..-." -> "F"
-        "--." -> "G"
-        "...." -> "H"
-        ".." -> "I"
-        ".---" -> "J"
-        "-.-" -> "K"
-        ".-.." -> "L"
-        "--" -> "M"
-        "-." -> "N"
-        "---" -> "O"
-        ".--." -> "P"
-        "--.-" -> "Q"
-        ".-." -> "R"
-        "..." -> "S"
-        "-" -> "T"
-        "..-" -> "U"
-        "...-" -> "V"
-        ".--" -> "W"
-        "-..-" -> "X"
-        "-.--" -> "Y"
-        "--.." -> "Z"
-        ".----" -> "1"
-        "..---" -> "2"
-        "...--" -> "3"
-        "....-" -> "4"
-        "....." -> "5"
-        "-...." -> "6"
-        "--..." -> "7"
-        "---.." -> "8"
-        "----." -> "9"
-        "-----" -> "0"
-        "" -> " "
-        ".-.-.-" -> "."
-        "--..--" -> ","
-        "---..." -> "."
-        "..--.." -> "?"
-        "-.-.--" -> "!"
-        "...---..." -> "SOS"
-        "-....-" -> "''"
-        "-..-." -> "/"
-        "-.--.-" -> "()"
-        ".--.-." -> "@"
-        "-...-" -> "="
-        else -> null
-    }
