369764e7e3070e08cccf0ba1133409daf459dc8c
ch8n <chetan.garg36@gmail.com>
2021-02-19 23:50:55 +0530

    added patterns

--- src/main/kotlin/final_450/printSqaure.kt
+++ src/main/kotlin/final_450/printSqaure.kt
@@ -1,10 +1,201 @@
 package final_450
 
 fun main() {
-    printSquareFilled(10, 5)
-    printSquareNumber(10, 5)
-    printSquareHollow(10, 5)
+//    printSquareFilled(10, 5)
+//    printSquareNumber(10, 5)
+//    printSquareHollow(10, 5)
+    //printOne()
+    // printTwo(10)
+    // printThree(9)
+    printFour(9)
 }
+
+
+fun printOne() {
+
+    /*
+    *  12345
+    *  1234
+    *  123
+    *  12
+    *  1
+     */
+
+    (5 downTo 1).forEach { height ->
+        println()
+        (1..height).forEach { width ->
+            print(width)
+        }
+    }
+
+
+}
+
+fun printTwo(n: Int) {
+
+    /*
+    *  1
+    *  12
+    *  123
+    *  1234
+    *  12345
+    *  1234
+    *  123
+    *  12
+    *  1
+    *
+     */
+
+    (1..n).forEach { height ->
+        println()
+        (1..height).forEach { width ->
+            print(width)
+        }
+    }
+
+    (n - 1 downTo 1).forEach { height ->
+        println()
+        (1..height).forEach { width ->
+            print(width)
+        }
+    }
+
+
+}
+
+fun printThree(n: Int) {
+
+    /*
+     *     1
+     *    12
+     *   123
+     *  1234
+     * 12345
+     */
+
+    (1..n).forEach { height ->
+        println()
+        ((n - height) downTo 1).forEach { space ->
+            print("*")
+        }
+        (1..height).forEach { number ->
+            print(number)
+        }
+    }
+
+
+}
+
+fun printFour(n: Int) {
+
+    /*
+     *    1
+     *   12
+     *  123
+     * 1234
+     *12345
+     * 1234
+     *  123
+     *   12
+     *    1
+     *
+     */
+    (5 downTo 1).forEach { height ->
+        (1..height).forEach {
+            print("*")
+        }
+        (5 - height downTo 1).forEach {
+            print(it)
+        }
+        println()
+
+    }
+
+    (1..4).forEach { height ->
+        (1..height + 1).forEach {
+            print("*")
+        }
+        (4 - height downTo 1).forEach {
+            print(it)
+        }
+        println()
+    }
+
+}
+
+
+/***
+ *  12345
+ *  1234
+ *  123
+ *  12
+ *  1
+ *
+ *  1
+ *  12
+ *  123
+ *  1234
+ *  12345
+ *  1234
+ *  123
+ *  12
+ *  1
+ *
+ *          1
+ *         23
+ *        123
+ *       1234
+ *      12345
+ *
+ *          1
+ *         23
+ *        123
+ *       1234
+ *      12345
+ *       1234
+ *        123
+ *         12
+ *          1
+ *
+ *
+ *           1
+ *          121
+ *         12321
+ *        1234321
+ *       123454321
+ *
+ *           1
+ *          121
+ *         12321
+ *        1234321
+ *       123454321
+ *        1234321
+ *         12321
+ *          121
+ *           1
+ *
+ *     55555
+ *    44444
+ *   33333
+ *  2222
+ * 1111
+ *
+ * 0
+ * 10
+ * 010
+ * 1010
+ * 01010
+ * 101010
+ *
+ *       1
+ *     1 2 3
+ *   1 2 3 4 5
+ *     1 2 3
+ *       1
+ *
+ *      *       *
+ *  *       *       *
+ */
 
 fun printSquareFilled(height: Int, width: Int) {
     repeat(height) { _ ->
