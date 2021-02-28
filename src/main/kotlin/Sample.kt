21085c54214968f3d0952b5806d08532d84607e4
ch8n <chetan.garg36@gmail.com>
2021-02-28 23:52:18 +0530

    primitive types Kotlin

--- src/main/kotlin/Sample.kt
+++ src/main/kotlin/Sample.kt
@@ -1,3 +1,26 @@
 fun main(){
-    println("hello world!")
+    // int
+    val test1 = 1
+    val test2 = 1
+    println(test1 === test2)
+    // double
+    val test3 = 1.0
+    val test4 = 1.0
+    println(test3 === test4)
+    // float
+    val test5 = 1.0f
+    val test6 = 1.0f
+    println(test5 === test6)
+    // string
+    val test7 = "1.2f"
+    val test8 = "1.0f"
+    println(test7 === test8)
+    // boolean
+    val test9 = true
+    val test10 = true
+    println(test9 === test10)
+    // char
+    val test11 = 'c'
+    val test12 = 'c'
+    println(test11 === test12)
 }
