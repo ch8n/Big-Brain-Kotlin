0b6459456d32bd9f82d69666d08b79caa4a259a5
ch8n <chetan.garg36@gmail.com>
2024-07-22 19:36:26 +0530

    wip

--- src/main/kotlin/2/neetcode/blind75/Main.kt
+++ src/main/kotlin/2/neetcode/blind75/Main.kt
@@ -1,9 +1,10 @@
 package `2`.neetcode.blind75
 
+import kotlin.math.max
+
 
 private fun main() {
-    val neetCodeBlind75 = NeetCodeBlind75()
-    with(neetCodeBlind75) {
+    with(NeetCodeBlind75.Hashing) {
         containDuplicate()
         isAnagram()
         twoSum()
@@ -11,190 +12,369 @@ private fun main() {
         topKFrequent()
         encodeDecode()
         productOfSelf()
+        longestSequence()
+    }
+
+    with(NeetCodeBlind75.TwoPointer) {
+        validatePalindrome()
+        threeSumToZero()
+        containMostWater()
+    }
+
+    with(NeetCodeBlind75.SlidingWindow) {
+        buyAndSellStock()
     }
 }
 
 
-private class NeetCodeBlind75() {
+private object NeetCodeBlind75 {
 
-    fun containDuplicate() {
-        // https://leetcode.com/problems/contains-duplicate/description/
+    object Hashing {
+        fun containDuplicate() {
+            // https://leetcode.com/problems/contains-duplicate/description/
 
-        fun solution(input: List<Int>) {
-            val isContainingDuplicate = input.toSet().size != input.size
-            println("input $input | isContainingDuplicate $isContainingDuplicate")
-        }
-
-        val inputs = listOf(
-            listOf(1, 2, 3, 1),
-            listOf(1, 2, 3, 4),
-            listOf(1, 1, 1, 3, 3, 4, 3, 2, 4, 2),
-        )
-
-        inputs.onEach { solution(it) }
-    }
-
-    fun isAnagram() {
-        //https://leetcode.com/problems/valid-anagram/description/
-        fun solution(input1: String, input2: String) {
-            val chars = Array(256) { 0 }
-            input1.onEach {
-                val count = chars.get(it.code)
-                chars.set(it.code, count + 1)
+            fun solution(input: List<Int>) {
+                val isContainingDuplicate = input.toSet().size != input.size
+                println("input $input | isContainingDuplicate $isContainingDuplicate")
             }
 
-            input2.onEach {
-                val count = chars.get(it.code)
-                chars.set(it.code, count - 1)
-            }
+            val inputs = listOf(
+                listOf(1, 2, 3, 1),
+                listOf(1, 2, 3, 4),
+                listOf(1, 1, 1, 3, 3, 4, 3, 2, 4, 2),
+            )
 
-            val isAnagram = chars.all { it == 0 }
-            println("inputs $input1, $input2 | isAnagram:$isAnagram")
+            inputs.onEach { solution(it) }
         }
 
-        val inputs = listOf(
-            listOf("anagram", "nagaram"),
-            listOf("rat", "car")
-        )
-
-        inputs.onEach {
-            solution(it.get(0), it.get(1))
-        }
-    }
-
-    fun twoSum() {
-        //https://leetcode.com/problems/two-sum/
-
-        fun solution(input: List<Int>, target: Int) {
-            val acc = hashMapOf<Int, Int>()
-            val result = mutableListOf<Int>()
-            input.onEachIndexed() { index, first ->
-                val other = target - first
-                val otherIndex = acc.get(other) ?: -1
-                if (otherIndex != -1) {
-                    result.add(index)
-                    result.add(otherIndex)
-                    return@onEachIndexed
+        fun isAnagram() {
+            //https://leetcode.com/problems/valid-anagram/description/
+            fun solution(input1: String, input2: String) {
+                val chars = Array(256) { 0 }
+                input1.onEach {
+                    val count = chars.get(it.code)
+                    chars.set(it.code, count + 1)
                 }
-                acc.put(first, index)
+
+                input2.onEach {
+                    val count = chars.get(it.code)
+                    chars.set(it.code, count - 1)
+                }
+
+                val isAnagram = chars.all { it == 0 }
+                println("inputs $input1, $input2 | isAnagram:$isAnagram")
             }
-            println("input : $input - target $target result $result")
-        }
 
-        val inputs = listOf(
-            listOf(3, 4, 5, 6) to 7,
-            listOf(4, 5, 6) to 10,
-            listOf(5, 5) to 10,
-            listOf(2, 7, 11, 15) to 9,
-            listOf(3, 2, 4) to 6,
-            listOf(3, 3) to 6
-        )
+            val inputs = listOf(
+                listOf("anagram", "nagaram"),
+                listOf("rat", "car")
+            )
 
-        inputs.onEach { (input, target) ->
-            solution(input, target)
-        }
-    }
-
-    fun groupAnagram() {
-        //https://leetcode.com/problems/group-anagrams/description/
-        fun solution(input: List<String>) {
-            val result = input
-                .groupBy { it.toCharArray().sorted() }
-                .values
-            println("input:$input \n result:$result")
-        }
-
-        val inputs = listOf(
-            listOf("eat", "tea", "tan", "ate", "nat", "bat"),
-            listOf(""),
-            listOf("a")
-        )
-
-        inputs.onEach {
-            solution(it)
-        }
-    }
-
-    fun topKFrequent() {
-        //https://leetcode.com/problems/top-k-frequent-elements/description/
-        fun solution(input: List<Int>, target: Int) {
-            val bucket = Array(input.size + 1) { 0 }
-            val groups = input.groupBy { it }
-            groups.onEach { (num, count) ->
-                bucket.set(count.size, num)
+            inputs.onEach {
+                solution(it.get(0), it.get(1))
             }
-            val result = bucket.filter { it != 0 }.takeLast(target)
-            println("input:$input topK:$target = $result")
         }
 
+        fun twoSum() {
+            //https://leetcode.com/problems/two-sum/
 
-        val inputs = listOf(
-            listOf(1, 1, 1, 2, 2, 3) to 2,
-            listOf(1) to 1
-        )
+            fun solution(input: List<Int>, target: Int) {
+                val acc = hashMapOf<Int, Int>()
+                val result = mutableListOf<Int>()
+                input.onEachIndexed() { index, first ->
+                    val other = target - first
+                    val otherIndex = acc.get(other) ?: -1
+                    if (otherIndex != -1) {
+                        result.add(index)
+                        result.add(otherIndex)
+                        return@onEachIndexed
+                    }
+                    acc.put(first, index)
+                }
+                println("input : $input - target $target result $result")
+            }
 
-        inputs.onEach { (input, target) ->
-            solution(input, target)
+            val inputs = listOf(
+                listOf(3, 4, 5, 6) to 7,
+                listOf(4, 5, 6) to 10,
+                listOf(5, 5) to 10,
+                listOf(2, 7, 11, 15) to 9,
+                listOf(3, 2, 4) to 6,
+                listOf(3, 3) to 6
+            )
+
+            inputs.onEach { (input, target) ->
+                solution(input, target)
+            }
         }
-    }
 
-    fun encodeDecode() {
-        fun solution(input: List<String>) {
-            val encode = input.joinToString("%20")
-            val decode = encode.split("%20")
-            println("encoded $encode -> decoded $decode")
+        fun groupAnagram() {
+            //https://leetcode.com/problems/group-anagrams/description/
+            fun solution(input: List<String>) {
+                val result = input
+                    .groupBy { it.toCharArray().sorted() }
+                    .values
+                println("input:$input \n result:$result")
+            }
+
+            val inputs = listOf(
+                listOf("eat", "tea", "tan", "ate", "nat", "bat"),
+                listOf(""),
+                listOf("a")
+            )
+
+            inputs.onEach {
+                solution(it)
+            }
         }
 
-        val input = listOf(
-            listOf("neet", "code", "love", "you"),
-            listOf("we", "say", ":", "yes")
-        )
-        input.onEach { solution(it) }
-    }
-
-    fun productOfSelf() {
-
-        fun solution(input: List<Int>) {
-
-            val pre = Array(input.size) { 0 }
-            var current = 1
-            for (index in 0..input.lastIndex) {
-                current *= input.get(index)
-                pre.set(index, current)
+        fun topKFrequent() {
+            //https://leetcode.com/problems/top-k-frequent-elements/description/
+            fun solution(input: List<Int>, target: Int) {
+                val bucket = Array(input.size + 1) { 0 }
+                val groups = input.groupBy { it }
+                groups.onEach { (num, count) ->
+                    bucket.set(count.size, num)
+                }
+                val result = bucket.filter { it != 0 }.takeLast(target)
+                println("input:$input topK:$target = $result")
             }
 
 
-            val post = Array(input.size) { 0 }
-            current = 1
-            for (index in input.lastIndex downTo 0) {
-                current *= input.get(index)
-                post.set(index, current)
-            }
+            val inputs = listOf(
+                listOf(1, 1, 1, 2, 2, 3) to 2,
+                listOf(1) to 1
+            )
 
-            val result = Array(input.size) { 0 }
-            for (index in 0..result.lastIndex) {
-                val prev = pre.getOrNull(index - 1) ?: 1
-                val next = post.getOrNull(index + 1) ?: 1
-                val product = prev * next
-                result.set(index, product)
+            inputs.onEach { (input, target) ->
+                solution(input, target)
             }
-
-            println("input:$input selfProduct:${result.joinToString()}")
         }
 
-        val inputs = listOf(
-            listOf(1, 2, 3, 4),
-            listOf(-1, 1, 0, -3, 3),
-        )
+        fun encodeDecode() {
+            fun solution(input: List<String>) {
+                val encode = input.joinToString("%20")
+                val decode = encode.split("%20")
+                println("encoded $encode -> decoded $decode")
+            }
 
-        inputs.onEach {
-            solution(it)
+            val input = listOf(
+                listOf("neet", "code", "love", "you"),
+                listOf("we", "say", ":", "yes")
+            )
+            input.onEach { solution(it) }
+        }
+
+        fun productOfSelf() {
+
+            fun solution(input: List<Int>) {
+
+                val pre = Array(input.size) { 0 }
+                var current = 1
+                for (index in 0..input.lastIndex) {
+                    current *= input.get(index)
+                    pre.set(index, current)
+                }
+
+
+                val post = Array(input.size) { 0 }
+                current = 1
+                for (index in input.lastIndex downTo 0) {
+                    current *= input.get(index)
+                    post.set(index, current)
+                }
+
+                val result = Array(input.size) { 0 }
+                for (index in 0..result.lastIndex) {
+                    val prev = pre.getOrNull(index - 1) ?: 1
+                    val next = post.getOrNull(index + 1) ?: 1
+                    val product = prev * next
+                    result.set(index, product)
+                }
+
+                println("input:$input selfProduct:${result.joinToString()}")
+            }
+
+            val inputs = listOf(
+                listOf(1, 2, 3, 4),
+                listOf(-1, 1, 0, -3, 3),
+            )
+
+            inputs.onEach {
+                solution(it)
+            }
+        }
+
+        fun longestSequence() {
+            //https://leetcode.com/problems/longest-consecutive-sequence/description/
+            fun solution(input: List<Int>) {
+                val itemIndexed = buildMap {
+                    input.onEachIndexed { index, item -> put(item, index) }
+                }
+                var maxCount = 0
+                input.onEach { entry ->
+                    val prev = entry - 1
+                    if (itemIndexed.containsKey(prev)) return@onEach
+                    var current = entry
+                    var count = 0
+                    while (itemIndexed.containsKey(current)) {
+                        count += 1
+                        current += 1
+                    }
+                    maxCount = maxOf(maxCount, count)
+                }
+
+                println("input $input, longestSequenceCount $maxCount")
+            }
+
+            val inputs = listOf(
+                listOf(100, 4, 200, 1, 3, 2),
+                listOf(0, 3, 7, 2, 5, 8, 4, 6, 0, 1),
+            )
+            inputs.onEach {
+                solution(it)
+            }
         }
     }
 
-    fun longestSequence() {
+    object TwoPointer {
 
+        fun validatePalindrome() {
+            //https://leetcode.com/problems/valid-palindrome/description/
+
+            fun solution(input: String) {
+                val normalized = input.lowercase().filter { it.isLetterOrDigit() }
+                val reversed = normalized.reversed()
+                val isPalindrome = normalized == reversed
+                println("input $input isPalindrome? $isPalindrome")
+            }
+
+            val inputs = listOf(
+                "A man, a plan, a canal: Panama",
+                "race a car",
+                " "
+            )
+
+            inputs.onEach { solution(it) }
+        }
+
+        fun threeSumToZero() {
+            //https://leetcode.com/problems/3sum/description/
+            fun solution(input: List<Int>) {
+
+                val sorted = input.sorted()
+                val result = mutableListOf<Triple<Int, Int, Int>>()
+                sorted.onEachIndexed { firstIndex, first ->
+                    val prev = sorted.getOrNull(firstIndex - 1)
+                    if (prev == first) return@onEachIndexed
+
+                    var start = firstIndex + 1
+                    var end = sorted.lastIndex
+
+                    while (start < end) {
+                        val second = sorted.get(start)
+                        val third = sorted.get(end)
+                        val sum = first + second + third
+                        println("sum $sum start:$start end:$end")
+                        when {
+                            sum > 0 -> end -= 1
+                            sum < 0 -> start += 1
+                            else -> {
+                                result.add(Triple(first, second, third))
+                                var current = sorted.get(start)
+                                while (current == second && start < end) {
+                                    start += 1
+                                    current = sorted.get(start)
+                                }
+
+                                current = sorted.get(end)
+                                while (current == third && start < end) {
+                                    end -= 1
+                                    current = sorted.get(end)
+                                }
+                            }
+                        }
+                    }
+                }
+
+                println("input $input -> sumToZeros@:$result")
+            }
+
+            val input = listOf(
+                listOf(-1, 0, 1, 2, -1, -4),
+                listOf(0, 1, 1),
+                listOf(0, 0, 0)
+            )
+
+            input.onEach { solution(it) }
+        }
+
+        fun containMostWater() {
+            // https://leetcode.com/problems/container-with-most-water/description/
+
+            fun solution(input: List<Int>) {
+                var start = 0
+                var end = input.lastIndex
+                var maxArea = 0
+                while (start < end) {
+                    val tower1 = input.get(start)
+                    val tower2 = input.get(end)
+                    val length = minOf(tower1, tower2)
+                    val breath = end - start
+                    val area = length * breath
+                    maxArea = maxOf(maxArea, area)
+                    if (tower1 > tower2) {
+                        end -= 1
+                    } else {
+                        start += 1
+                    }
+                }
+
+                println("towers: $input, maxArea: $maxArea")
+            }
+
+            val inputs = listOf(
+                listOf(1, 8, 6, 2, 5, 4, 8, 3, 7), // 49
+                listOf(1, 1), // 1
+                listOf(1, 7, 2, 5, 4, 7, 3, 6), // 36
+                listOf(2, 2, 2) //4
+            )
+
+            inputs.onEach { solution(it) }
+        }
     }
 
+    object SlidingWindow {
 
+        fun buyAndSellStock() {
+            //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+            fun solution(input: List<Int>) {
+                var buy = 0
+                var sell = buy + 1
+
+                var maxProfit = 0
+                while (sell < input.lastIndex) {
+                    val buyValue = input.get(buy)
+                    val sellValue = input.get(sell)
+                    val profit = maxOf(sellValue - buyValue, 0)
+                    maxProfit = maxOf(maxProfit, profit)
+                    if (buyValue > sellValue) {
+                        buy += 1
+                        sell = buy + 1
+                    } else {
+                        sell += 1
+                    }
+                }
+
+                println("input $input, maxProfit $maxProfit")
+            }
+
+            val inputs = listOf(
+                listOf(7, 1, 5, 3, 6, 4),//5
+                listOf(7, 6, 4, 3, 1),//0
+            )
+            inputs.onEach { solution(it) }
+        }
+    }
 }
--- src/main/kotlin/2/neetcode/neetcode150/Main.kt
+++ src/main/kotlin/2/neetcode/neetcode150/Main.kt
@@ -2,55 +2,123 @@ package `2`.neetcode.neetcode150
 
 
 private fun main() {
-    val neetCode150 = NeetCode150()
-    with(neetCode150) {
+    with(NeetCode150.Hashing) {
         validateSudoku()
+    }
+
+    with(NeetCode150.TwoPointers) {
+        trappingWater()
     }
 }
 
-private class NeetCode150 {
-    fun validateSudoku() {
-        fun solution(input: List<List<String>>) {
+private object NeetCode150 {
+    object Hashing {
+        fun validateSudoku() {
             //https://leetcode.com/problems/valid-sudoku/description/
-            val rowMatrix = Array(9) { mutableListOf<String>() }
-            val colMatrix = Array(9) { mutableListOf<String>() }
-            val boxMatrix = Array(9) { mutableListOf<String>() }
 
-            for (rowIndex in 0 until 9) {
-                for (colIndex in 0 until 9) {
-                    val boxIndex = ((rowIndex / 3) * 3) + (colIndex / 3)
-                    val entry = input.get(rowIndex).get(colIndex)
-                    if (entry == ".") continue
-                    rowMatrix.get(rowIndex).add(entry)
-                    colMatrix.get(colIndex).add(entry)
-                    boxMatrix.get(boxIndex).add(entry)
+            fun solution(input: List<List<String>>) {
+                val rowMatrix = Array(9) { mutableListOf<String>() }
+                val colMatrix = Array(9) { mutableListOf<String>() }
+                val boxMatrix = Array(9) { mutableListOf<String>() }
+
+                for (rowIndex in 0 until 9) {
+                    for (colIndex in 0 until 9) {
+                        val boxIndex = ((rowIndex / 3) * 3) + (colIndex / 3)
+                        val entry = input.get(rowIndex).get(colIndex)
+                        if (entry == ".") continue
+                        rowMatrix.get(rowIndex).add(entry)
+                        colMatrix.get(colIndex).add(entry)
+                        boxMatrix.get(boxIndex).add(entry)
+                    }
                 }
+
+                val isRowValid = rowMatrix.all { enties -> enties.size == enties.toSet().size }
+                val isColValid = colMatrix.all { enties -> enties.size == enties.toSet().size }
+                val isBoxValid = boxMatrix.all { enties -> enties.size == enties.toSet().size }
+                println("row->$isRowValid col->$isColValid box->$isBoxValid")
+                println(isRowValid && isColValid && isBoxValid)
             }
 
-            val isRowValid = rowMatrix.all { enties -> enties.size == enties.toSet().size }
-            val isColValid = colMatrix.all { enties -> enties.size == enties.toSet().size }
-            val isBoxValid = boxMatrix.all { enties -> enties.size == enties.toSet().size }
-            println("row->$isRowValid col->$isColValid box->$isBoxValid")
-            println(isRowValid && isColValid && isBoxValid)
-        }
 
-
-        val inputs = listOf(
-            listOf(
-                listOf("5", "3", ".", ".", "7", ",", ".", ".", "."),
-                listOf("6", ".", ".", "1", "9", "5", ".", ".", "."),
-                listOf(".", "9", "8", ".", ".", ".", ".", "6", "."),
-                listOf("8", ".", ".", ".", "6", ".", ".", ".", "3"),
-                listOf("4", ".", ".", "8", ".", "3", ".", ".", "1"),
-                listOf("7", ".", ".", ".", "2", ".", ".", ".", "6"),
-                listOf(".", "6", ".", ".", ".", ".", "2", "8", "."),
-                listOf(".", ".", ".", "4", "1", "9", ".", ".", "5"),
-                listOf(".", ".", ".", ".", "8", ".", ".", "7", "9"),
+            val inputs = listOf(
+                listOf(
+                    listOf("5", "3", ".", ".", "7", ",", ".", ".", "."),
+                    listOf("6", ".", ".", "1", "9", "5", ".", ".", "."),
+                    listOf(".", "9", "8", ".", ".", ".", ".", "6", "."),
+                    listOf("8", ".", ".", ".", "6", ".", ".", ".", "3"),
+                    listOf("4", ".", ".", "8", ".", "3", ".", ".", "1"),
+                    listOf("7", ".", ".", ".", "2", ".", ".", ".", "6"),
+                    listOf(".", "6", ".", ".", ".", ".", "2", "8", "."),
+                    listOf(".", ".", ".", "4", "1", "9", ".", ".", "5"),
+                    listOf(".", ".", ".", ".", "8", ".", ".", "7", "9"),
+                )
             )
-        )
 
-        inputs.onEach {
-            solution(it)
+            inputs.onEach {
+                solution(it)
+            }
+        }
+    }
+
+    object TwoPointers {
+        fun trappingWater() {
+
+            fun solution1(input: List<Int>) {
+                val leftMax = Array(input.size) { 0 }
+                val rightMax = Array(input.size) { 0 }
+
+                var max = 0
+                for (index in 0..input.lastIndex) {
+                    max = maxOf(max, input.get(index))
+                    leftMax.set(index, max)
+                }
+
+                max = 0
+                for (index in input.lastIndex downTo 0) {
+                    max = maxOf(max, input.get(index))
+                    rightMax.set(index, max)
+                }
+
+                val boundary = (0..input.lastIndex).map {
+                    minOf(leftMax.get(it), rightMax.get(it))
+                }
+
+                val waterTrapped = input.mapIndexed { index, height ->
+                    maxOf(boundary.get(index) - height, 0)
+                }.sum()
+
+                println("soln1 | input $input, water trapped $waterTrapped")
+            }
+
+            fun solution2(heights: List<Int>) {
+                var start = 0
+                var end = heights.lastIndex
+                var leftMax = heights.get(start)
+                var rightMax = heights.get(end)
+                val waterTrapped = mutableListOf<Int>()
+                while (start < end) {
+                    if (leftMax <= rightMax) {
+                        start += 1
+                        leftMax = maxOf(leftMax, heights.get(start))
+                        waterTrapped += maxOf(0, leftMax - heights.get(start))
+                    } else {
+                        end -= 1
+                        rightMax = maxOf(rightMax, heights.get(end))
+                        waterTrapped += maxOf(0, rightMax - heights.get(end))
+                    }
+                }
+                println("soln2 | input $heights , water trapped ${waterTrapped.sum()}")
+            }
+
+            val inputs = listOf(
+                listOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1),//6
+                listOf(4, 2, 0, 3, 2, 5)//9
+            )
+
+            inputs
+                .onEach { solution1(it) }
+                .onEach { solution2(it) }
+
         }
     }
 }
